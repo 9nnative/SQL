a74ab1d726d841a7730a487505ae369b177bc376
Corentin <66473604+9nnative@users.noreply.github.com>
2020-07-28 12:00:08 +0200

    pushLogin

---
+++ login/index.php
@@ -0,0 +1,42 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p id="message">
+        <?php
+            if(isset($_SESSION['success'])){
+                echo $_SESSION['success'];
+                unset($_SESSION['success']);
+            }
+        ?>
+    </p>
+    <div>
+        <?php
+            require "DAO.php";
+            $pdo = connect();
+
+            $stmt = $pdo->query("SELECT * FROM user");
+
+            $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            echo "<table><thead><tr><th>Pseudo</th><th>E-mail</th><th>Date d'inscription</th></tr></thead>";
+            foreach($users as $user){
+                echo "<tr>";
+                echo "<td>".$user['username']."</td>";
+                echo "<td>".$user['email']."</td>";
+                echo "<td>".$user['createdAt']."</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        ?>
+    </div>
+    <a href="form_register.php">S'inscrire</a>
+    <a href="form_login.php">Se connecter</a>
+</body>
+</html>
